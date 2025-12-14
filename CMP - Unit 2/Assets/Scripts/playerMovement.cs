a491a0906ff2bc7b6d663b9a6290e1e0798eb2b8
scoutfrfr <scoutbotha@gmail.com>
2025-12-14 16:35:27 +0000

    Jumping animation
    Made an animation for the players jump mechanic to make it more visually appealing.

--- CMP - Unit 2/Assets/Scripts/playerMovement.cs
+++ CMP - Unit 2/Assets/Scripts/playerMovement.cs
@@ -5,12 +5,13 @@ using static Unity.VisualScripting.Member;
 public class playerMovement : MonoBehaviour
 {
     // Declaring public variables
-    private Rigidbody2D rb;
     public float playerSpeed;
     public float jumpHeight;
 
 
     // Declaring private variables
+    private Rigidbody2D rb;
+    private Animator anim;
     private float movementX;
     private float movementY;
 
@@ -20,6 +21,7 @@ public class playerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -35,7 +37,7 @@ public class playerMovement : MonoBehaviour
         {
             // Jump mechanic 
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
-            playerGrounded = false; 
+            playerGrounded = false;
         }
 
         // Makes it so if player holds space they jump higher whereas if they just press space and release immediately the jump is shorter
@@ -44,6 +46,15 @@ public class playerMovement : MonoBehaviour
             // Jump mechanic 
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
             playerGrounded = false;
+        }
+
+        if (playerGrounded == false)
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
         }
     }
 
